d3b8c852d5b7a48afbecad0d828b2cdb036848aa
wkblev01 <129453825+wkblev01@users.noreply.github.com>
2023-04-05 23:22:16 -0400

    Add files via upload

---
+++ CSharp-Program-Delivery-Cost-Form/Isolated Written Code Example/DeliveryCostCalculator.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Program2
+{
+    partial class DeliveryCostCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weightLbl = new System.Windows.Forms.Label();
+            this.distLbl = new System.Windows.Forms.Label();
+            this.daysLbl = new System.Windows.Forms.Label();
+            this.weightTxtBox = new System.Windows.Forms.TextBox();
+            this.distTxtBox = new System.Windows.Forms.TextBox();
+            this.daysTxtBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.aCostLbl = new System.Windows.Forms.Label();
+            this.bCostLbl = new System.Windows.Forms.Label();
+            this.cCostLbl = new System.Windows.Forms.Label();
+            this.aCostOutputLbl = new System.Windows.Forms.Label();
+            this.bCostOutputLbl = new System.Windows.Forms.Label();
+            this.cCostOutputLbl = new System.Windows.Forms.Label();
+            this.lowestCostOutputLbl = new System.Windows.Forms.Label();
+            this.lowestCostLbl = new System.Windows.Forms.Label();
+            this.headerLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // weightLbl
+            // 
+            this.weightLbl.AutoSize = true;
+            this.weightLbl.Location = new System.Drawing.Point(32, 46);
+            this.weightLbl.Name = "weightLbl";
+            this.weightLbl.Size = new System.Drawing.Size(90, 13);
+            this.weightLbl.TabIndex = 0;
+            this.weightLbl.Text = "Package Weight:";
+            // 
+            // distLbl
+            // 
+            this.distLbl.AutoSize = true;
+            this.distLbl.Location = new System.Drawing.Point(32, 75);
+            this.distLbl.Name = "distLbl";
+            this.distLbl.Size = new System.Drawing.Size(90, 13);
+            this.distLbl.TabIndex = 1;
+            this.distLbl.Text = "Distance in Miles:";
+            // 
+            // daysLbl
+            // 
+            this.daysLbl.AutoSize = true;
+            this.daysLbl.Location = new System.Drawing.Point(42, 101);
+            this.daysLbl.Name = "daysLbl";
+            this.daysLbl.Size = new System.Drawing.Size(75, 13);
+            this.daysLbl.TabIndex = 2;
+            this.daysLbl.Text = "Delivery Days:";
+            // 
+            // weightTxtBox
+            // 
+            this.weightTxtBox.Location = new System.Drawing.Point(123, 43);
+            this.weightTxtBox.Name = "weightTxtBox";
+            this.weightTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.weightTxtBox.TabIndex = 3;
+            // 
+            // distTxtBox
+            // 
+            this.distTxtBox.Location = new System.Drawing.Point(123, 72);
+            this.distTxtBox.Name = "distTxtBox";
+            this.distTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.distTxtBox.TabIndex = 4;
+            // 
+            // daysTxtBox
+            // 
+            this.daysTxtBox.Location = new System.Drawing.Point(123, 98);
+            this.daysTxtBox.Name = "daysTxtBox";
+            this.daysTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.daysTxtBox.TabIndex = 5;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(45, 134);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(158, 23);
+            this.calcButton.TabIndex = 6;
+            this.calcButton.Text = "Calculate Costs";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // aCostLbl
+            // 
+            this.aCostLbl.AutoSize = true;
+            this.aCostLbl.Location = new System.Drawing.Point(29, 178);
+            this.aCostLbl.Name = "aCostLbl";
+            this.aCostLbl.Size = new System.Drawing.Size(88, 13);
+            this.aCostLbl.TabIndex = 7;
+            this.aCostLbl.Text = "Company A Cost:";
+            // 
+            // bCostLbl
+            // 
+            this.bCostLbl.AutoSize = true;
+            this.bCostLbl.Location = new System.Drawing.Point(29, 213);
+            this.bCostLbl.Name = "bCostLbl";
+            this.bCostLbl.Size = new System.Drawing.Size(88, 13);
+            this.bCostLbl.TabIndex = 8;
+            this.bCostLbl.Text = "Company B Cost:";
+            // 
+            // cCostLbl
+            // 
+            this.cCostLbl.AutoSize = true;
+            this.cCostLbl.Location = new System.Drawing.Point(29, 249);
+            this.cCostLbl.Name = "cCostLbl";
+            this.cCostLbl.Size = new System.Drawing.Size(88, 13);
+            this.cCostLbl.TabIndex = 9;
+            this.cCostLbl.Text = "Company C Cost:";
+            // 
+            // aCostOutputLbl
+            // 
+            this.aCostOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.aCostOutputLbl.Location = new System.Drawing.Point(123, 177);
+            this.aCostOutputLbl.Name = "aCostOutputLbl";
+            this.aCostOutputLbl.Size = new System.Drawing.Size(88, 20);
+            this.aCostOutputLbl.TabIndex = 10;
+            // 
+            // bCostOutputLbl
+            // 
+            this.bCostOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bCostOutputLbl.Location = new System.Drawing.Point(123, 212);
+            this.bCostOutputLbl.Name = "bCostOutputLbl";
+            this.bCostOutputLbl.Size = new System.Drawing.Size(88, 18);
+            this.bCostOutputLbl.TabIndex = 11;
+            // 
+            // cCostOutputLbl
+            // 
+            this.cCostOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cCostOutputLbl.Location = new System.Drawing.Point(123, 248);
+            this.cCostOutputLbl.Name = "cCostOutputLbl";
+            this.cCostOutputLbl.Size = new System.Drawing.Size(88, 20);
+            this.cCostOutputLbl.TabIndex = 12;
+            // 
+            // lowestCostOutputLbl
+            // 
+            this.lowestCostOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lowestCostOutputLbl.Location = new System.Drawing.Point(168, 299);
+            this.lowestCostOutputLbl.Name = "lowestCostOutputLbl";
+            this.lowestCostOutputLbl.Size = new System.Drawing.Size(88, 20);
+            this.lowestCostOutputLbl.TabIndex = 13;
+            // 
+            // lowestCostLbl
+            // 
+            this.lowestCostLbl.AutoSize = true;
+            this.lowestCostLbl.Location = new System.Drawing.Point(12, 300);
+            this.lowestCostLbl.Name = "lowestCostLbl";
+            this.lowestCostLbl.Size = new System.Drawing.Size(150, 13);
+            this.lowestCostLbl.TabIndex = 14;
+            this.lowestCostLbl.Text = "The Lowest Cost Company is: ";
+            // 
+            // headerLbl
+            // 
+            this.headerLbl.AutoSize = true;
+            this.headerLbl.Location = new System.Drawing.Point(30, 13);
+            this.headerLbl.Name = "headerLbl";
+            this.headerLbl.Size = new System.Drawing.Size(198, 13);
+            this.headerLbl.TabIndex = 15;
+            this.headerLbl.Text = "Please enter your shipping details below:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 349);
+            this.Controls.Add(this.headerLbl);
+            this.Controls.Add(this.lowestCostLbl);
+            this.Controls.Add(this.lowestCostOutputLbl);
+            this.Controls.Add(this.cCostOutputLbl);
+            this.Controls.Add(this.bCostOutputLbl);
+            this.Controls.Add(this.aCostOutputLbl);
+            this.Controls.Add(this.cCostLbl);
+            this.Controls.Add(this.bCostLbl);
+            this.Controls.Add(this.aCostLbl);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.daysTxtBox);
+            this.Controls.Add(this.distTxtBox);
+            this.Controls.Add(this.weightTxtBox);
+            this.Controls.Add(this.daysLbl);
+            this.Controls.Add(this.distLbl);
+            this.Controls.Add(this.weightLbl);
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label weightLbl;
+        private System.Windows.Forms.Label distLbl;
+        private System.Windows.Forms.Label daysLbl;
+        private System.Windows.Forms.TextBox weightTxtBox;
+        private System.Windows.Forms.TextBox distTxtBox;
+        private System.Windows.Forms.TextBox daysTxtBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label aCostLbl;
+        private System.Windows.Forms.Label bCostLbl;
+        private System.Windows.Forms.Label cCostLbl;
+        private System.Windows.Forms.Label aCostOutputLbl;
+        private System.Windows.Forms.Label bCostOutputLbl;
+        private System.Windows.Forms.Label cCostOutputLbl;
+        private System.Windows.Forms.Label lowestCostOutputLbl;
+        private System.Windows.Forms.Label lowestCostLbl;
+        private System.Windows.Forms.Label headerLbl;
+    }
+}
+
---
+++ CSharp-Program-Delivery-Cost-Form/Isolated Written Code Example/DeliveryCostCalculator.cs
@@ -0,0 +1,174 @@
+﻿// Lab 
+// CIS 199-01
+// Due: 10/15/2020
+// By: R1466
+
+// This program creates a form to accept shipping details such as mile, weight, and delivery days.
+// Form uses nested if statements to calculate and display shipping costs for three companies, then outputs which shipping cost is lowest.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class DeliveryCostCalculator : Form
+    {
+        // Variables
+        private bool validInput;    // Boolean variable to hold if TryParse was successful.
+        private int days;           // Holds input for delivery days in form. Must be an integer. Used in individual companies cost calculation.
+        private double weight,      // Holds input for package weight in form. Could be fractional. Used in individual companies cost calculation.
+        miles,                      // Holds input for miles traveled in form. Could be fractional. Used in individual companies cost calculation.
+        aCost,                      // Holds calculation for Company A's delivery cost. Used for output and in if statement to determine lowest cost.
+        bCost,                      // Holds calculation for Company B's delivery cost. Used for output and in if statement to determine lowest cost.
+        cCost;                      // Holds calculation for Company C's delivery cost. Used for output and in if statement to determine lowest cost.
+
+        public DeliveryCostCalculator()
+        {
+            InitializeComponent();
+        }
+
+        // Event that triggers upon activation of calcButton.
+        // This event uses a series of nested if statements to calculate and display shipping costs for three companies.
+        // Input is validated with TryParse, costs are calculated, displayed, and lowest cost company is displayed.
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // Constant Variables
+            const int VALID_INPUT_FLOOR = 0;            // Constant to test if inputs are above 0.
+
+            const double A_MILE_RATE = 0.02;            // Company A's mile rate of $0.02 per.
+            const int A_WEIGHT_RATE = 1;                // Company A's weight rate of $1 per lb.
+            const int A_DAY_RATE_1 = 20;                // Company A's flat rate for 1 day delivery.
+            const int A_DAY_RATE_2 = 17;                // Company A's flat rate for 2 day delivery.
+            const int A_DAY_RATE_3 = 15;                // Company A's flat rate for 3 day delivery.
+            const int A_DAY_RATE_4_6 = 10;              // Company A's flat rate for 4-6 day delivery.
+            const int A_DAY_RATE_7 = 7;                 // Company A's flat rate for 7+ day delivery.
+            const int A_DAY_7 = 7;                      // Constant to test if Company A's 7+ day delivery applies.
+            const int A_DAY_4_6 = 1;                    // Constant to test if Company A's 4-6 day delivery applies.
+            const int A_DAY_3 = 3;                      // Constant to test if Company A's 3 day delivery applies.
+            const int A_DAY_2 = 2;                      // Constant to test if Company A's 2 day delivery applies. If all tests fail, 1 day delivery applies.
+
+
+            const double B_MILE_RATE = 0.1;             // Company B's mile rate of $0.02 per.
+            const int B_DAY_RATE_1_3 = 10;              // Company B's flat rate for 1-3 day delivery.
+            const int B_DAY_RATE_4 = 7;                 // Company B's flat rate for 4+ day delivery.
+            const int B_DAY_4 = 4;                      // Constant to test if Company B's 4+ day delivery applies. If test fails, 1-3 day delivery applies.
+            const int B_WEIGHT_RATE_UP_TO_10 = 3;       // Company B's flat rate for weight under 10 lbs.
+            const int B_WEIGHT_RATE_UP_TO_50 = 5;       // Company B's flat rate for weight 10 to up to 50 lbs.
+            const int B_WEIGHT_RATE_UP_TO_100 = 10;     // Company B's flat rate for weight 50 to up to 100 lbs.
+            const int B_WEIGHT_RATE_UP_TO_200 = 20;     // Company B's flat rate for weight 100 to up to 200 lbs.
+            const double B_WEIGHT_RATE_200 = 0.15;      // Company B's rate for weight 200 lbs and up.
+            const int B_WEIGHT_200 = 200;               // Constant to test if Company B's 200+ weight rate applies.
+            const int B_WEIGHT_UP_TO_200 = 100;         // Constant to test if Company B's 100 to up to 200 lbs weight rate applies.
+            const int B_WEIGHT_UP_TO_100 = 50;          // Constant to test if Company B's 50 to up to 100 lbs weight rate applies.
+            const int B_WEIGHT_UP_TO_50 = 10;           // Constant to test if Company B's 10 to up to 50 lbs weight rate applies. If all tests fail, under 10 lbs weight rate applies.
+
+            const double C_WEIGHT_RATE = 0.25;          // Company C's weight rate of $0.25 per lb.
+            const int C_DAY_RATE = 20;                  // Company C's flat rate for delivery of any day amount.
+            const int C_MILE_RATE_UP_TO_200 = 10;       // Company C's flat rate for miles under 200.
+            const int C_MILE_RATE_UP_TO_500 = 15;       // Company C's flat rate for miles 200 up to 500.
+            const int C_MILE_RATE_UP_TO_750 = 25;       // Company C's flat rate for miles 500 up to 750.
+            const int C_MILE_RATE_UP_TO_1000 = 35;      // Company C's flat rate for miles 750 up to 1000.
+            const int C_MILE_RATE_1000 = 40;            // Company C's flat rate for miles 1000 and above.
+            const int C_MILE_1000 = 1000;               // Constant to test if Company C's 1000+ mile rate applies.
+            const int C_MILE_UP_TO_1000 = 750;          // Constant to test if Company C's 750 up to 1000 mile rate applies.
+            const int C_MILE_UP_TO_750 = 500;           // Constant to test if Company C's 500 up to 750 mile rate applies.
+            const int C_MILE_UP_TO_500 = 200;           // Constant to test if Company C's 200 up to 500 mile rate applies. If all tests fail, under 200 mile rate applies.
+
+
+            // TryParse for valid inputs.
+            if (int.TryParse(daysTxtBox.Text, out days) && days > VALID_INPUT_FLOOR)                    // Checks if days input is an integer and is above 0.
+                if (double.TryParse(distTxtBox.Text, out miles) && miles > VALID_INPUT_FLOOR)           // Checks if miles input is a double and is above 0.
+                    if (double.TryParse(weightTxtBox.Text, out weight) && weight > VALID_INPUT_FLOOR)   // Checks if weight input is a double and is above 0.
+                        validInput = true;                      // True if all TryParse tests are passed. Used to trigger rest of if statements
+                    else
+                        MessageBox.Show("Invalid Weight.");     // Shows error message if weight input is not valid.
+                else
+                    MessageBox.Show("Invalid Distance.");       // Shows error message if miles input is not valid.
+            else
+                MessageBox.Show("Invalid Delivery Days.");      // Shows error message if days input is not valid.
+
+
+            // Nested if statements to calculate and display cost for Company A.
+            if (validInput == true)
+            {
+                if (days >= A_DAY_7)
+                    aCost = (miles * A_MILE_RATE) + (weight * A_WEIGHT_RATE) + (A_DAY_RATE_7);
+                else if (days >= A_DAY_4_6)
+                    aCost = (miles * A_MILE_RATE) + (weight * A_WEIGHT_RATE) + (A_DAY_RATE_4_6);
+                else if (days == A_DAY_3)
+                    aCost = (miles * A_MILE_RATE) + (weight * A_WEIGHT_RATE) + (A_DAY_RATE_3);
+                else if (days == A_DAY_2)
+                    aCost = (miles * A_MILE_RATE) + (weight * A_WEIGHT_RATE) + (A_DAY_RATE_2);
+                else
+                    aCost = (miles * A_MILE_RATE) + (weight * A_WEIGHT_RATE) + (A_DAY_RATE_1);
+
+                aCostOutputLbl.Text = $"{aCost.ToString("C")}";     // Outputs cost of Company A to label on form.
+            }
+
+            // Nested if statements to calculate and display cost for Company B.
+            if (validInput == true)
+            {
+                if (days >= B_DAY_4)
+                    if (weight >= B_WEIGHT_200)
+                        bCost = (miles * B_MILE_RATE) + (weight * B_WEIGHT_RATE_200) + (B_DAY_RATE_4);
+                    else if (weight >= B_WEIGHT_UP_TO_200)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_200) + (B_DAY_RATE_4);
+                    else if (weight >= B_WEIGHT_UP_TO_100)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_100) + (B_DAY_RATE_4);
+                    else if (weight >= B_WEIGHT_UP_TO_50)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_50) + (B_DAY_RATE_4);
+                    else
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_10) + (B_DAY_RATE_4);
+
+                else
+                    if (weight >= B_WEIGHT_200)
+                        bCost = (miles * B_MILE_RATE) + (weight * B_WEIGHT_RATE_200) + (B_DAY_RATE_1_3);
+                    else if (weight >= B_WEIGHT_UP_TO_200)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_200) + (B_DAY_RATE_1_3);
+                    else if (weight >= B_WEIGHT_UP_TO_100)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_100) + (B_DAY_RATE_1_3);
+                    else if (weight >= B_WEIGHT_UP_TO_50)
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_50) + (B_DAY_RATE_1_3);
+                    else
+                        bCost = (miles * B_MILE_RATE) + (B_WEIGHT_RATE_UP_TO_10) + (B_DAY_RATE_1_3);
+
+                bCostOutputLbl.Text = $"{bCost.ToString("C")}";     // Outputs cost of Company B to label on form.
+            }
+
+            // If statement to calculate and display cost for Company C.
+            if (validInput == true)
+            {
+                if (miles >= C_MILE_1000)
+                    cCost = (weight * C_WEIGHT_RATE) + (C_DAY_RATE) + (C_MILE_RATE_1000);
+                else if (miles >= C_MILE_UP_TO_1000)
+                    cCost = (weight * C_WEIGHT_RATE) + (C_DAY_RATE) + (C_MILE_RATE_UP_TO_1000);
+                else if (miles >= C_MILE_UP_TO_750)
+                    cCost = (weight * C_WEIGHT_RATE) + (C_DAY_RATE) + (C_MILE_RATE_UP_TO_750);
+                else if (miles >= C_MILE_UP_TO_500)
+                    cCost = (weight * C_WEIGHT_RATE) + (C_DAY_RATE) + (C_MILE_RATE_UP_TO_500);
+                else
+                    cCost = (weight * C_WEIGHT_RATE) + (C_DAY_RATE) + (C_MILE_RATE_UP_TO_200);
+
+                cCostOutputLbl.Text = $"{cCost.ToString("C")}";     // Outputs cost of Company C to label on form.
+            }
+
+            // Nested if statment to caculate and display lowest cost company.
+            if (validInput == true)
+            {
+                if ((aCost < bCost) && (aCost < cCost))
+                    lowestCostOutputLbl.Text = "Company A";     // Outputs to label on form if Company A is lowest.
+                else if ((bCost < aCost) && (bCost < cCost))
+                    lowestCostOutputLbl.Text = "Company B";     // Outputs to label on form if Company B is lowest.
+                else
+                    lowestCostOutputLbl.Text = "Company C";     // Outputs to label on form if Company C is lowest.
+            }
+        }
+    }
+}
